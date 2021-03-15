89f38301de512d6357b0dc99ccdbb939d5861060
cameronsassman <71071769+cameronsassman@users.noreply.github.com>
2021-03-15 09:38:40 +0200

    Delete hamburger.js

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-function togglemenu() {
-  let menu = document.getElementById("nav");
-
-  menu.style.display === "block"
-    ? (menu.style.display = "none")
-    : (menu.style.display = "block");
-}
