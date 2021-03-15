9bfead8ef8cd9a7b8f59d6e228ee4687338b5b14
cameronsassman <71071769+cameronsassman@users.noreply.github.com>
2021-03-15 09:41:26 +0200

    Delete navbar.js

--- navbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $(".topnav").addClass("sticky");
-        } else {
-            $(".topnav").removeClass("sticky");
-        }
-    })
-}
